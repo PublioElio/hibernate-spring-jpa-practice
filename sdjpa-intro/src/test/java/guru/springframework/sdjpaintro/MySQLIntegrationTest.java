acbff59e3b928eb4e3ca44523d7d08e2b4504fc3
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-05-18 01:19:20 +0200

    Update MySQLIntegrationTest.java

--- sdjpa-intro/src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ sdjpa-intro/src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,9 +1,9 @@
 package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.repositories.BookRepository;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,7 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("local")
 @DataJpaTest
 @ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
-public class MySQLIntegrationTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
