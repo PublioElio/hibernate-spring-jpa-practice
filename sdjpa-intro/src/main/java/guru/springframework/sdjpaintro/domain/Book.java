d262bae13b55b42676ec5a223b55e377b7a8a4d9
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-06-22 21:38:24 +0200

    additional changes in liquibase book_seq id

--- sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -7,7 +7,8 @@ import jakarta.persistence.*;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_seq")
+    @SequenceGenerator(name = "book_seq", sequenceName = "book_seq", allocationSize = 1)
     private Long id;
     private String title;
     private String publisher;
