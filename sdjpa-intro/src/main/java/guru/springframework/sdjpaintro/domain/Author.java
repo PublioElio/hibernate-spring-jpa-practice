fbc605921f02bb518d5ac2aaa4753afe47fb3c14
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-06-22 22:53:25 +0200

    created new entity Author

---
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -0,0 +1,58 @@
+package guru.springframework.sdjpaintro.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+    @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 1)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    public Author() {
+    }
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id.equals(author.id);
+    }
+
+    @Override
+    public int hashCode() { return 0; }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
